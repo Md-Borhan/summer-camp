151c1989d726d4d7dcd34dd5230d40c09d641788
Borhan Uddin <borhanm373@gmail.com>
2023-06-11 19:47:03 +0600

    student selected class implement

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+/* import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useAuth } from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import Loader from "../../loader/Loader";
+import { useEffect } from "react";
 
 const Classes = () => {
   const { user, loading } = useAuth();
@@ -21,7 +22,16 @@ const Classes = () => {
     return res.data;
   });
 
-  const handleSelectButton = (user) => {
+  const handleSelectButton = (user, sc) => {
+    useEffect(()=>{
+      fetch(`${import.meta.env.VITE_api_url}/bookedClasses`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(sc)
+      });
+    },[sc])
     if (!user) {
       Swal.fire({
         title: "Please login first to select button.",
@@ -72,7 +82,117 @@ const Classes = () => {
                     users.role === "admin" ||
                     users.role === "instructor"
                   }
-                  onClick={() => handleSelectButton(user)}
+                  onClick={() => handleSelectButton(user, sc)}
+                  className="btn btn-block rounded-full border-transparent bg-[#571ce0] hover:bg-transparent px-5 text-white hover:border-[#571ce0]"
+                >
+                  Select Class
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Classes;
+ */
+
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useAuth } from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Loader from "../../loader/Loader";
+import { toast } from "react-hot-toast";
+
+const Classes = () => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: classes = [] } = useQuery(["classes"], async () => {
+    const res = await axiosSecure.get("/classes");
+    return res.data;
+  });
+
+  const { data: users = [] } = useQuery(["users"], async () => {
+    const res = await axiosSecure.get(`/users/${user?.email}`);
+    return res.data;
+  });
+
+  const handleSelectButton = (user, sc) => {
+    if (!user) {
+      Swal.fire({
+        title: "Please login first to select button.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login", { state: { from: location } });
+        }
+      });
+    } else {
+      fetch(`${import.meta.env.VITE_api_url}/booked`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sc),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            toast.success("Class Booked!");
+          }
+        });
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div className="text-white">
+      <div className="grid grid-cols-1 my-14 md:grid-cols-2 lg:grid-cols-3 gap-8 myContainer">
+        {classes?.map((sc) => (
+          <div
+            key={sc._id}
+            className="card w-full text-white border-[#571ce0] shadow-blue-100 shadow bg-base-100 "
+          >
+            <figure>
+              <img src={sc.imageUrl} alt="sport" />
+            </figure>
+            <div
+              className={`card-body rounded-b-2xl ${
+                sc.seats === 0 ? "bg-red-500" : "bg-[#1F2340]"
+              }`}
+            >
+              <h2 className="card-title font-bold">{sc.className}</h2>
+              <p className="font-medium">Instructor Name: {sc.name}</p>
+              <div className="flex items-center justify-between">
+                <p className="font-medium">
+                  Available Seats:
+                  <span className="font-normal"> {sc.seats}</span>
+                </p>
+                <p className="font-medium">
+                  Price: $<span className="font-normal"> {sc.price}</span>
+                </p>
+              </div>
+              <div className="border mt-4 w-full p-1 rounded-full inline-block border-[#571ce0] shadow-blue-100 shadow">
+                <button
+                  disabled={
+                    sc.seats === 0 ||
+                    users?.role === "admin" ||
+                    users?.role === "instructor"
+                  }
+                  onClick={() => handleSelectButton(user, sc)}
                   className="btn btn-block rounded-full border-transparent bg-[#571ce0] hover:bg-transparent px-5 text-white hover:border-[#571ce0]"
                 >
                   Select Class
--- src/pages/Dashboard/MySelectedClass.jsx
+++ src/pages/Dashboard/MySelectedClass.jsx
@@ -1,10 +1,76 @@
+import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../components/SectionTitle";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const MySelectedClass = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: bookedClasses = [] } = useQuery(["BookedClasses"], async () => {
+    const res = await axiosSecure.get("/booked");
+    return res.data;
+  });
+
+  const handleDelete = (bookedClass) => {
+    console.log(bookedClass);
+  };
+  const handlePay = (bookedClass) => {
+    console.log(bookedClass);
+  };
   return (
     <div className="text-white">
       <SectionTitle title="My Selected Class" />
-      <h2></h2>
+      <div className="overflow-x-auto bg-[#322a71] p-10 rounded-md">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="text-white border-[#571ce0]">
+              <th>S/L</th>
+              <th>Image</th>
+              <th>Class Name</th>
+              <th>Instructor Name</th>
+              <th>Seats</th>
+              <th>Price</th>
+              <th>Delete</th>
+              <th>Pay</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookedClasses?.map((bookedClass, index) => (
+              <tr key={bookedClass._id} className="border-b border-[#571ce0]">
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="rounded-full  border-[#571ce057] shadow-blue-100 shadow border w-12 h-12">
+                        <img src={bookedClass.imageUrl} alt="Avatar" />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{bookedClass.className}</td>
+                <td>{bookedClass.name}</td>
+                <td>{bookedClass.seats}</td>
+                <td className="text-right">$ {bookedClass.price}</td>
+                <td className="py-2 px-4">
+                  <button
+                    className="btn btn-sm bg-red-500 text-xs text-white hover:bg-[#1F2340]"
+                    onClick={() => handleDelete(bookedClass)}
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td className="py-2 px-4">
+                  <button
+                    className="btn btn-sm bg-[#1F2340] text-xs text-white hover:bg-[#1F2340]"
+                    onClick={() => handlePay(bookedClass)}
+                  >
+                    Pay
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
